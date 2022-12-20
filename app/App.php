7aa61b24c32d669b71d2ad615fde48f6246963da
Arman Fayziev <arman.fayziev@scandiweb.com>
2022-12-20 04:05:33 +0200

    app dockerizing

---
+++ app/App.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class App
+{
+    public function __construct()
+    {
+        echo 'I am from App class';
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\App;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new App();
+$app;
+
