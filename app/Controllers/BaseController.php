2453132bb902ba285115f1f88edfa1ee81714030
Arman Fayziev <arman.fayziev@scandiweb.com>
2022-12-20 04:19:30 +0200

    controller register added

---
+++ app/Controllers/BaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Controllers;
+
+class BaseController
+{
+    public function index()
+    {
+        echo 'BaseController';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,9 +1,19 @@
 <?php
 
-use App\App;
+declare(strict_types = 1);
+
+use App\Router;
+use App\Controllers\BaseController;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new App();
-$app;
+$router = new Router();
 
+$router
+    ->get('/', [BaseController::class, 'index'])
+    ;
+
+echo $router->resolve(
+    $_SERVER['REQUEST_URI'],
+    strtolower($_SERVER['REQUEST_METHOD'])
+);
